d08fec7952a13b48cbbcc53a7fe7ac94823034cc
Braqzen <103777923+Braqzen@users.noreply.github.com>
2024-11-10 14:32:28 +0700

    Length and signature seem to not error

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
 use alloy::{
-    consensus::{SignableTransaction, TxEip1559},
-    primitives::{address, b256, hex, Signature, TxKind, U256},
-    rlp::Encodable,
+    consensus::{SignableTransaction, TxEip1559, TxEnvelope},
+    eips::eip2718::{Decodable2718, Encodable2718},
+    primitives::{address, hex, keccak256, PrimitiveSignature, TxKind, U256},
+    rlp::{BytesMut, Decodable, Encodable},
 };
 use rand::Rng;
 use reqwest::{Client, Error};
-use secp256k1::{Message, PublicKey, Secp256k1, SecretKey, SignOnly};
+use secp256k1::{ecdsa::RecoveryId, All, Message, PublicKey, Secp256k1, SecretKey};
 use serde_json::json;
 use sha3::{Digest, Keccak256};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+const GENESIS_TIMESTAMP: u64 = 1606824023; // Genesis time (Dec 1, 2020, 12:00:23 PM UTC)
+const SLOT_DURATION: u64 = 12;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // TODO: lazy, just put the port in manually after you start the devnet
-    let port = 32793;
+    let port = 32791;
     let client = Client::new();
-    let signature = Signature::from_scalars_and_parity(
-        b256!("840cfc572845f5786e702984c2a582528cad4b49b2a10b9db1be7fca90058565"),
-        b256!("25e7109ceb98168d95b09b18bbf6b685130e0562f233877d492b94eee0c5b6d1"),
-        false,
-    )?;
 
-    let tx = create_tx();
-    let raw_tx = convert_to_raw_tx(tx.clone(), signature);
+    let secp = Secp256k1::new();
+    let secret_key = SecretKey::from_byte_array(&[0xcd; 32]).expect("32 bytes, within curve order");
+    let public_key = PublicKey::from_secret_key(&secp, &secret_key).serialize_uncompressed();
 
-    match send_request(port, &client, raw_tx, tx).await {
+    let (tx, raw) = create_tx(secret_key);
+
+    // Seems to decode properly
+    // raw_decode(raw.clone());
+
+    let current_time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let slot_time = current_time + 3;
+    let slot = (slot_time - GENESIS_TIMESTAMP) / SLOT_DURATION;
+
+    let digest = message_digest(tx, slot);
+    let header = create_header(&secret_key, &public_key, &secp, &digest);
+
+    match send_request(port, &client, (raw, slot, header)).await {
         Ok(response) => {
             println!("Response: {:?}", response);
         }
@@ -35,29 +51,32 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn raw_decode(raw: String) {
+    let tx = raw.trim_start_matches("0x");
+    let tx_bytes = hex::decode(tx).expect("Failed bytes");
+
+    let envelope = TxEnvelope::decode_2718(&mut &tx_bytes[..]).expect("Failed 2718");
+    dbg!(envelope);
+}
+
 async fn send_request(
     port: u64,
     client: &Client,
-    raw_tx: String,
-    tx: TxEip1559,
+    data: (String, u64, String),
 ) -> Result<serde_json::Value, Error> {
-    let slot = 23;
-    let digest = message_digest(tx, slot);
-    let header = create_header(&digest);
-
     let request_body = json!({
         "jsonrpc": "2.0",
         "method": "bolt_requestInclusion",
         "params":  vec![json!({
-            "txs": vec![raw_tx],
-            "slot": slot
+            "txs": vec![data.0],
+            "slot": data.1
         })],
         "id": 1,
     });
 
     let response = client
         .post(format!("http://127.0.0.1:{}", port))
-        .header("X-Bolt-Signature", header)
+        .header("X-Bolt-Signature", data.2)
         .json(&request_body)
         .send()
         .await?;
@@ -66,10 +85,10 @@ async fn send_request(
     Ok(json_response)
 }
 
-fn create_tx() -> TxEip1559 {
+fn create_tx(secret_key: SecretKey) -> (TxEnvelope, String) {
     let mut rng = rand::thread_rng();
 
-    TxEip1559 {
+    let tx = TxEip1559 {
         chain_id: 1,
         nonce: rng.gen_range(1..=u64::MAX),
         gas_limit: rng.gen_range(21000..=100000),
@@ -78,42 +97,52 @@ fn create_tx() -> TxEip1559 {
         max_fee_per_gas: rng.gen_range(1..=100000),
         max_priority_fee_per_gas: rng.gen_range(1..=100000),
         ..Default::default()
-    }
-}
+    };
 
-fn convert_to_raw_tx(tx: TxEip1559, signature: Signature) -> String {
-    let mut rlp_encoded = Vec::new();
+    let secp = Secp256k1::new();
+    let mut buff = BytesMut::new();
+    tx.encode(&mut buff);
+    let tx_hash = keccak256(&buff);
+    let message = Message::from_digest(tx_hash.0);
+    let signature = secp.sign_ecdsa_recoverable(&message, &secret_key);
+    let (recovery_id, sig) = signature.serialize_compact();
+    let (r, s) = sig.split_at(32);
+    let r = U256::from_be_slice(r);
+    let s = U256::from_be_slice(s);
+    let v = match recovery_id {
+        RecoveryId::Zero => true,
+        RecoveryId::One => false,
+        _ => panic!("Unsupported recovery ID"),
+    };
+
+    let signature = PrimitiveSignature::new(r, s, v);
+
     let signed_tx = tx.into_signed(signature);
-    signed_tx.tx().encode(&mut rlp_encoded);
-    format!("0x{}", hex::encode(rlp_encoded))
+    let envelope = TxEnvelope::Eip1559(signed_tx);
+    let raw = hex::encode_prefixed(envelope.encoded_2718());
+
+    (envelope, raw)
 }
 
-fn message_digest(tx: TxEip1559, target_slot: u64) -> Message {
-    let mut rlp_encoded = Vec::new();
-    tx.encode(&mut rlp_encoded);
-    let tx_hash = Keccak256::digest(rlp_encoded);
+fn message_digest(tx: TxEnvelope, target_slot: u64) -> Message {
+    let message_digest = {
+        let mut data = Vec::new();
 
-    let hash_len = tx_hash.as_slice().len();
-    let slot_len = std::mem::size_of::<u64>();
+        data.extend_from_slice(tx.tx_hash().as_slice());
+        data.extend_from_slice(&target_slot.to_le_bytes());
 
-    let mut data = vec![0u8; hash_len + slot_len];
-    data[..hash_len].copy_from_slice(tx_hash.as_slice());
-    data[hash_len..].copy_from_slice(&target_slot.to_le_bytes());
+        Keccak256::digest(data)
+    };
 
-    let mut hasher = Keccak256::new();
-    hasher.update(data);
-    let hash = hasher.finalize();
-
-    let mut digest = [0u8; 32];
-    digest.copy_from_slice(&hash[..]);
-
-    Message::from_digest(digest)
+    Message::from_digest(message_digest.into())
 }
 
-fn create_header(digest: &Message) -> String {
-    let secp: Secp256k1<SignOnly> = Secp256k1::gen_new();
-    let secret_key = SecretKey::from_slice(&[1u8; 32]).expect("Failed to load secret key");
-    let public_key = PublicKey::from_secret_key(&secp, &secret_key).serialize_uncompressed();
+fn create_header(
+    secret_key: &SecretKey,
+    public_key: &[u8; 65],
+    secp: &Secp256k1<All>,
+    digest: &Message,
+) -> String {
     let public_key = &public_key[1..];
 
     let mut hasher = Keccak256::new();
